51c3c6e650b6837459d875734e98bce3f00f3df8
gok2 <goktug.balaban29@gmail.com>
2023-11-20 14:56:14 +0300

    seperate GradientContainer class to another file

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(48, 7, 119, 1),
+            Color.fromRGBO(114, 52, 221, 1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 42,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,31 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(48, 7, 119, 1),
-            Color.fromRGBO(114, 52, 221, 1),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 42,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
-  }
 }
